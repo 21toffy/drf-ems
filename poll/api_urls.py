4ecb9ce1b3c341cfcf2c3996e2990efd1d23432b
21toffy <oketofoke@gmail.com>
2020-08-22 22:05:27 +0100

    done with APIviews

--- poll/api_urls.py
+++ poll/api_urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('poll', poll) 
+    path('poll', PollAPIView.as_view()),
+    path('poll/<int:id>', PollDetailAPIView.as_view()) 
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,12 +1,56 @@
 from django.shortcuts import render
 from .models import Question
 from .serializers import QuestionSerializer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+
+
+
+class PollAPIView(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data, status=200)
+    def post(self, request):
+        data = request.data
+        serializer =  QuestionSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class PollDetailAPIView(APIView):
+    def get_object(self, id):
+        try:
+            return Question.objects.get(id=id)
+        except Question.DoesNotExist as e:
+            return Response({'error':'Given question object was not found.'}, status=404)
+    def get(self,request, id=None):
+        instance=self.get_object(id)
+        serializer = QuestionSerializer(instance)
+        return Response(serializer.data)
+
+    def put(self, request, id=None):
+        data = request.data
+        instance = self.get_object(id)
+        serializer = QuestionSerializer(instance, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, id=None):
+        instance = self.get_object(id)
+        instance.delete()
+        return Response("data":"data deleted", status=201)
+
 
 
 # @api_view(['GET'])
